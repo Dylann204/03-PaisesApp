bf3938dd14382bbe9d34d04f787230adfb20fefb
dylan <dylan204.play@gmail.com>
2022-02-17 12:15:03 -0500

    Se suben cambios

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [],
+})
+export class PorPaisComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+  constructor(private pasisService: PaisService) {}
+
+  buscar() {
+    this.hayError = false;
+    console.log(this.termino);
+
+    this.pasisService.searchCountry(this.termino).subscribe({
+      next: (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      error: () => {
+        this.hayError = true;
+        this.paises = [];
+      },
+    });
+  }
+}
